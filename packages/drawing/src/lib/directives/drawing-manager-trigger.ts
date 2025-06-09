99ce0fc3452052602582e4b61ec86520c59f5b99
babluroy <bablu.roy@batchservice.com>
2025-06-09 14:05:46 +0530

    drawing bugs fixes

--- packages/drawing/src/lib/directives/drawing-manager-trigger.ts
+++ packages/drawing/src/lib/directives/drawing-manager-trigger.ts
@@ -1,30 +1,30 @@
 import { AgmMap } from '@babluroy/agm-core';
 import { Directive, Host, Input, OnDestroy, OnInit, Inject } from '@angular/core';
+import { AgmDrawingManager } from './drawing-manager';
 
 declare var google: any;
 
 @Directive({
-  selector: 'agm-drawing-manager-trigger',
+  selector: 'agm-map[agmDrawingManager]',
   standalone: true,
 })
 export class AgmDrawingManagerTrigger implements OnInit, OnDestroy {
-  @Input() drawingMode: google.maps.drawing.OverlayType | null = null;
-  @Input() options: google.maps.drawing.DrawingManagerOptions = {};
-
-  private _drawingManager: google.maps.drawing.DrawingManager | null = null;
+  /** The drawing manager to be attached to this trigger. */
+  @Input() drawingManager!: AgmDrawingManager;
 
   constructor(@Host() @Inject(AgmMap) private _agmMap: AgmMap) {}
 
   ngOnInit() {
-    this._agmMap.mapReady.subscribe((map) => {
-      this._drawingManager = new google.maps.drawing.DrawingManager(this.options);
-      this._drawingManager?.setMap(map);
+    this._agmMap.mapReady.subscribe((map: google.maps.Map) => {
+      if (this.drawingManager) {
+        this.drawingManager.setMap(map);
+      }
     });
   }
 
   ngOnDestroy() {
-    if (this._drawingManager) {
-      this._drawingManager.setMap(null);
+    if (this.drawingManager) {
+      this.drawingManager.setMap(null as unknown as google.maps.Map);
     }
   }
 }
---
+++ temp-drawing-import-test.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Just a dummy usage to ensure types are resolved
+const module = undefined;
+const manager = undefined;
+console.log(module, manager);
